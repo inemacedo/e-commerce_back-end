9705f269a9914b6a6673cf33c9c157e4c2d730a6
Andy Nievas <nievasandy73@gmail.com>
2022-03-24 15:01:30 -0300

    cors

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const dbInitialSetup = require("./dbInitialSetup");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.use(express.json());
+app.use(cors());
 
 routes(app);
 
