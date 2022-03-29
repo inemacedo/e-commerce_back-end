b5badfe2b500f1a13e733460f8db935692fdf5ff
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-03-29 17:33:48 -0300

    corrección modelo Order

--- models/Order.js
+++ models/Order.js
@@ -1,33 +1,30 @@
 module.exports = (sequelize, Model, DataTypes) => {
   class Order extends Model {}
 
-  Order.init({
-    id: {
-      type: DataTypes.BIGINT.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true,
+  Order.init(
+    {
+      id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: ["OK", "ERROR", "PAID", "SENT", "CANCELLED"],
+        allowNull: false,
+      },
+      adress: {
+        type: DataTypes.STRING,
+      },
+      products: {
+        type: DataTypes.JSON,
+      },
     },
-    status: {
-      type: DataTypes.ENUM,
-      values: ["OK", "ERROR", "PAID", "SENT", "CANCELLED"],
-      allowNull: false,
+    {
+      sequelize,
+      modelName: "order",
     },
-    adress: {
-      type: DataTypes.STRING,
-    },
-    products: {
-      type: DataTypes.JSON,
-      //   get(){
-      //     return JSON.parse(this.getDataValue("products"))
-      //   },
-      //   set(value){
-      //     return (this.setDataValue("products",JSON.stringify))
-      //   }
-      // },
-    },
-    sequelize,
-    modelName: "order",
-  });
+  );
 
   return Order;
 };
