9d128b392bf120a9f93965902ac9f9a491cf1777
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-03-29 12:28:10 -0300

    nombre categories en URL

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,24 +3,24 @@ const checkJwt = require("express-jwt");
 const categoryController = require("../controllers/categoryController");
 const categoryRoutes = express.Router();
 
-categoryRoutes.get("/category", categoryController.getAll);
+categoryRoutes.get("/categories", categoryController.getAll);
 
-categoryRoutes.get("/category/:id", categoryController.getOne);
+categoryRoutes.get("/categories/:id", categoryController.getOne);
 
 categoryRoutes.post(
-  "/category",
+  "/categories",
   checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
   categoryController.store,
 );
 
 categoryRoutes.patch(
-  "/category/:id",
+  "/categories/:id",
   checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
   categoryController.update,
 );
 
 categoryRoutes.delete(
-  "/category/:id",
+  "/categories/:id",
   checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
   categoryController.destroy,
 );
