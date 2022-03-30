50abdc8612325d87bf22b4c0138aa07eebb3aa43
Valentina Giusti <valentinamgiusti@gmail.com>
2022-03-30 12:55:46 -0300

    productos nuevos

--- seeders/allProducts.js
+++ seeders/allProducts.js
@@ -178,6 +178,186 @@ const products = [
     slug: "butaca-glam",
     categoryId: 3,
   },
+  {
+    title: "Mesa Extensible Isbel",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/39df4d/www.kavehome.com.uy/kaveuy/97c5/webp/catalogo/CC000_7M40_2/900x1125/mesa-extensible-isbel-140-220-x-90-cm-madera-maciza-de-roble.jpg",
+    description:
+      "Tu comedor, una de las estancias centrales de tu casa, merece lo mejor. Por eso, empieza por una buena mesa. Apuesta por la madera maciza de roble de la mesa Isbel y sus extensiones para los que no pueden faltar en las mejores ocasiones.",
+    price: 1740,
+    image:
+      "https://f.fcdn.app/imgs/b09bd3/www.kavehome.com.uy/kaveuy/a224/webp/catalogo/CC000_7M40_1/900x1125/mesa-extensible-isbel-140-220-x-90-cm-madera-maciza-de-roble.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/950cae/www.kavehome.com.uy/kaveuy/6488/webp/catalogo/CC000_7M40_8/900x1125/mesa-extensible-isbel-140-220-x-90-cm-madera-maciza-de-roble.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Al 75 cm x An (140 - 220) cm x Pr 90 cm",
+    style: "Moderno, Rústico",
+    material: "	Madera maciza, Roble",
+    environment: "Comedor, Estar",
+    slug: "mesa-extensible-isbel",
+    categoryId: 1,
+  },
+  {
+    title: "Set Akemi De 2 Mesas De Centro",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/e3e8ee/www.kavehome.com.uy/kaveuy/52ab/webp/catalogo/CC1834C07_CC1834C07_2/900x1125/set-akemi-de-2-mesas-de-centro-set-akemi-de-2-mesas-de-centro.jpg",
+    description:
+      "Este set de vidrio texturizado es la pieza perfecta para los que soñaban con un salón de tendencia industrial y diseño geométrico. Con una mesa de 50 x 55 cm y otra de 100 x 50 cm, lo tienes todo para disfrutar de la estancia principal de tu casa.",
+    price: 470,
+    image:
+      "https://f.fcdn.app/imgs/4abc6c/www.kavehome.com.uy/kaveuy/75ac/webp/catalogo/CC1834C07_CC1834C07_1/900x1125/set-akemi-de-2-mesas-de-centro-set-akemi-de-2-mesas-de-centro.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/870302/www.kavehome.com.uy/kaveuy/e18e/webp/catalogo/CC1834C07_CC1834C07_6/900x1125/set-akemi-de-2-mesas-de-centro-set-akemi-de-2-mesas-de-centro.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Al 43/35 cm x An 100/50 cm x Pr 50/55 cm",
+    style: "Industrial, Moderno",
+    material: "Acero, Cristal",
+    environment: "Living",
+    slug: "set-akemi-de-2-mesas-de-centro",
+    categoryId: 1,
+  },
+  {
+    title: "Mesa Argo Roble Blanqueado",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/4b302d/www.kavehome.com.uy/kaveuy/60f2/webp/catalogo/C409M33_C409M33_2/900x1125/mesa-argo-180-x-100-cm-roble-blanqueado-patas-de-acero-acabado-negro-mesa-argo-180-x-100-cm-roble-blanqueado-patas-de-acero-acabado-negro.jpg",
+    description:
+      "Comidas familiares, ocasiones especiales, fiestas con los amigos… Los mejores momentos están alrededor de una buena mesa. Ten la mejor. Con Argo, de madera maciza de roble eco y patas de acero, añade diseño con los mejores materiales y acabados",
+    price: 2550,
+    image:
+      "https://f.fcdn.app/imgs/9b1cf5/www.kavehome.com.uy/kaveuy/e4d8/webp/catalogo/C409M33_C409M33_1/900x1125/mesa-argo-180-x-100-cm-roble-blanqueado-patas-de-acero-acabado-negro-mesa-argo-180-x-100-cm-roble-blanqueado-patas-de-acero-acabado-negro.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/4df30f/www.kavehome.com.uy/kaveuy/a151/webp/catalogo/C409M33_C409M33_5/900x1125/mesa-argo-180-x-100-cm-roble-blanqueado-patas-de-acero-acabado-negro-mesa-argo-180-x-100-cm-roble-blanqueado-patas-de-acero-acabado-negro.jpg",
+    stock: 1,
+    featured: false,
+    measures: "	Alto 78 cm x Ancho 180 cm x Prof. 100 cm",
+    style: "Moderno, Rústico",
+    material: "	Acero, Madera, Roble",
+    environment: "Comedor",
+    slug: "mesa-argo-roble-blanqueado",
+    categoryId: 1,
+  },
+  {
+    title: "Sofá Noa 3 Plazas",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/435826/www.kavehome.com.uy/kaveuy/1f1c/webp/catalogo/S719GC_02_2/900x1125/sofa-noa-3-plazas-con-almohadones-gris-patas-acabado-natural-230-cm.jpg",
+    description:
+      "La pieza principal de tu salón es eco. El sofá Noa tiene un diseño que, además de ofrecer confort con su efecto plumón, piensa en verde, ya que todo su tapizado se ha hecho 100% con PET, plástico reciclado recuperado del mar. Ideal para ti y tu ambiente.",
+    price: 3700,
+    image:
+      "https://f.fcdn.app/imgs/a0def9/www.kavehome.com.uy/kaveuy/1ff6/webp/catalogo/S719GC_02_1/900x1125/sofa-noa-3-plazas-con-almohadones-gris-patas-acabado-natural-230-cm.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/6e2311/www.kavehome.com.uy/kaveuy/28aa/webp/catalogo/S719GC_02_8/900x1125/sofa-noa-3-plazas-con-almohadones-gris-patas-acabado-natural-230-cm.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Alto 94 cm x Ancho 230 cm x Prof. 100 cm",
+    style: "Moderno, Nórdico",
+    material: "Espuma, Madera, Pino, Tela",
+    environment: "Escritorio, Living",
+    slug: "sofa-noa-3-plazas",
+    categoryId: 3,
+  },
+  {
+    title: "Sofá Galene De 3 Plazas",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/b35d28/www.kavehome.com.uy/kaveuy/9c05/webp/catalogo/S695GR39_S695GR39_2/900x1125/sofa-galene-de-3-plazas-beige-214-cm-sofa-galene-de-3-plazas-beige-214-cm.jpg",
+    description:
+      "Un buen diseño por dentro y por fuera. Con el sofá Galene, además de tener un extra confort gracias a su efecto plumón, le haces un bien al planeta, ya que todo su relleno se ha hecho con PET, plástico reciclado recuperado del mar.",
+    price: 2775,
+    image:
+      "https://f.fcdn.app/imgs/a24c9f/www.kavehome.com.uy/kaveuy/5597/webp/catalogo/S695GR39_S695GR39_1/900x1125/sofa-galene-de-3-plazas-beige-214-cm-sofa-galene-de-3-plazas-beige-214-cm.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/46ba53/www.kavehome.com.uy/kaveuy/d590/webp/catalogo/S695GR39_S695GR39_8/900x1125/sofa-galene-de-3-plazas-beige-214-cm-sofa-galene-de-3-plazas-beige-214-cm.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Alto 94 cm Ancho 214 cm Prof. 96 cm",
+    style: "Moderno, Nórdico",
+    material: "Poliester",
+    environment: "Living",
+    slug: "sofa-galene-de-3-plazas",
+    categoryId: 3,
+  },
+  {
+    title: "Sofá Obo 3 Plazas",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/6077f2/www.kavehome.com.uy/kaveuy/e2c0/webp/catalogo/S290J_14_2/900x1125/sofa-obo-3-plazas-gris-claro-190-cm.jpg",
+    description:
+      "Sofá de estilo escandinavo tapizado en tela modelo Miss. Pespuntes decorativos en el respaldo. Pies de madera natural",
+    price: 2080,
+    image:
+      "https://f.fcdn.app/imgs/8e54b1/www.kavehome.com.uy/kaveuy/36b7/webp/catalogo/S290J_14_1/900x1125/sofa-obo-3-plazas-gris-claro-190-cm.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/21ba5f/www.kavehome.com.uy/kaveuy/2fa8/webp/catalogo/S290J_14_8/900x1125/sofa-obo-3-plazas-gris-claro-190-cm.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Alto 81  cm x Ancho 190  cm x Prof. 81  cm",
+    style: "Minimalista, Moderno, Nórdico",
+    material: "Tapizado/a, Tela",
+    environment: "Estar, Living",
+    slug: "sofa-obo-3-plazas",
+    categoryId: 3,
+  },
+  {
+    title: "Silla Croft - Gris Claro",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/f5c44d/www.kavehome.com.uy/kaveuy/2481/webp/catalogo/CC0077JQ_03_2/900x1125/silla-croft-gris-claro-acabado-natural.jpg",
+    description:
+      "Silla con brazos. Tapizada en tela, parte posterior del asiento acolchado. Pies de madera maciza de fresno",
+    price: 525,
+    image:
+      "https://f.fcdn.app/imgs/25a4f6/www.kavehome.com.uy/kaveuy/aec9/webp/catalogo/CC0077JQ_03_1/900x1125/silla-croft-gris-claro-acabado-natural.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/cd280b/www.kavehome.com.uy/kaveuy/2889/webp/catalogo/CC0077JQ_03_7/900x1125/silla-croft-gris-claro-acabado-natural.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Alto 80 cm x Ancho 54 cm x Prof. 57 cm",
+    style: "Moderno, Nórdico",
+    material: "Antimanchas, Madera maciza, Tela",
+    environment: "Comedor, Escritorio",
+    slug: "silla-croft-gris-claro",
+    categoryId: 2,
+  },
+  {
+    title: "Silla Nayme - Beige",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/fd8cbd/www.kavehome.com.uy/kaveuy/2330/webp/catalogo/CC1344AB_12_2/900x1125/silla-nayme-beige.jpg",
+    description:
+      "Todo lo bueno ocurre alrededor de una mesa. Así que, escoge bien las sillas y apuesta por aquellas que más hablen de ti. La silla Nayme es exclusiva como tú, al ser de madera maciza de caucho, con tonalidades y vetas únicas",
+    price: 275,
+    image:
+      "https://f.fcdn.app/imgs/0157f2/www.kavehome.com.uy/kaveuy/ad50/webp/catalogo/CC1344AB_12_1/900x1125/silla-nayme-beige.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/923026/www.kavehome.com.uy/kaveuy/731e/webp/catalogo/CC1344AB_12_8/900x1125/silla-nayme-beige.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Alto 77 cm x Ancho 48 cm x Prof. 50 cm",
+    style: "Moderno, Nórdico",
+    material: "Caucho, Madera maciza",
+    environment: "Comedor",
+    slug: "silla-nayme-beige",
+    categoryId: 2,
+  },
+  {
+    title: "Silla Trise",
+    imageenvironment:
+      "https://f.fcdn.app/imgs/03f2cd/www.kavehome.com.uy/kaveuy/bf9b/webp/catalogo/CC0219M_46_2/900x1125/silla-trise-dm-y-madera-maciza-de-caucho-con-lacado-natural.jpg",
+    description:
+      "¿Comedor pequeño? Welcome! Gracias al diseño ligero de Trise, de madera con lacado natural, mantienes la luminosidad y sensación de amplitud en los espacios, además de añadirle estilo.",
+    price: 345,
+    image:
+      "https://f.fcdn.app/imgs/77de28/www.kavehome.com.uy/kaveuy/5960/webp/catalogo/CC0219M_46_1/900x1125/silla-trise-dm-y-madera-maciza-de-caucho-con-lacado-natural.jpg",
+    imagemeasures:
+      "https://f.fcdn.app/imgs/90c4f4/www.kavehome.com.uy/kaveuy/404a/webp/catalogo/CC0219M_46_5/900x1125/silla-trise-dm-y-madera-maciza-de-caucho-con-lacado-natural.jpg",
+    stock: 1,
+    featured: false,
+    measures: "Alto 77 cm x Ancho 53  cm x Prof. 54 cm",
+    style: "Vintage",
+    material: "Caucho, Madera maciza",
+    environment: "Comedor, Escritorio",
+    slug: "silla-trise",
+    categoryId: 2,
+  },
 ];
 
 module.exports = products;
