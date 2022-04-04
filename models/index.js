2e5ca45b0d8905ec2753380e0202ddd740c21a02
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-04-04 12:29:27 -0300

    supabase

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST, // Ej: 127.0.0.1
     dialect: process.env.DB_CONNECTION, // Ej: mysql
+    dialectModule: require("pg"),
     logging: false, // Para que no aparezcan mensajes en consola.
   },
 );
